98ce5cf113d5dc4f0d826c151f4f0144f3f052ff
Sophia <sophiambataru@gmail.com>
2023-03-20 08:21:01 +0300

    Complete

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 
 class User():
-    def __init__(self, name, age, gender):
+    def __init__(self, name, age):
         self.name = name
         self.age = age
     def show_details(self):
@@ -13,13 +13,13 @@ class Bank(User):
 
     def __init__(self, name, age, balance):
         super().__init__(name, age)
-        self.balance = 0
+        self.balance = balance
 
     def show_info(self):
         return f"{self.name.title()} has a remaining balance of: ${round(self.balance, 2)}"
     
     def deposit(self):
-        dp = float(input(f"{self.name.title()}, please enter how much you would like to deposit"))
+        dp = float(input(f"{self.name.title()}, please enter how much you would like to deposit: "))
         print("Thank you for depositing...")
         self.balance += dp
         self.total_deposits += 1
@@ -27,70 +27,84 @@ class Bank(User):
 
 
     def withdraws(self):
-        dp = float(input(f"{self.name.title()}, please enter how much you would like to withdraw"))
+        wd = float(input(f"{self.name.title()}, please enter how much you would like to withdraw: "))
         if self.balance < wd:
-            print(f"Insufficient Funds | Balance Available : ${self.balance}")
+            return "You can't withdraw that amount"
         else:
             print("Thank you for withdrawing...")
             self.balance -= wd
             self.total_withdraws += 1
+            return f"Your balance is now: {round(self.balance, 2)}"
 
 def options(user_two):
-    print("Thank you fro creating your bank account")
+    print("Thank you for creating your bank account")
     print("Here are a list of options, please choose the number you want")
+    
     while True:
-        option_choice = int(input("1) Balance\n2) Withdraw\3) Deposit\n4) Total Withdraws\n5) Total Deposits\n6) Quit\n"))
+        option_choice = int(input("1) Balance\n2) Withdraw\n3) Deposit\n4) Total Withdraws\n5) Total Deposits\n6) Quit\n"))
         if option_choice == 1:
             print(user_one_bank.show_info())
             if option_choice == 1 and user_two != None:
                 print(user_two_bank.show_info())
-            elif option_choice == 2:
-                print(user_one_bank.withdraws())
-                if option_choice == 2 and user_two != None:
-                    wd = input(f"{user_two.name} would you like to withdraw? Yes or No: ")
-                    if wd.lowere() == 'yes':
-                        print(user_two_bank.withdraws())
+        elif option_choice == 2:
+            print(user_one_bank.withdraws())
+            if option_choice == 2 and user_two != None:
+                wd = input(f"{user_two.name} would you like to withdraw? Yes or No: ")
+                if wd.lower() == 'yes':
+                    print(user_two_bank.withdraws())
 
-            elif option_choice == 3:
-                print(user_one_bank.deposit())
-                if option_choice == 3 and user_two != None:
-                    dep = input(f"{user_two.name} would you like to deposit? Yes or No: ")
-                    if dep.lower() == 'yes':
-                        print(user_two_bank.deposit())
-            elif option_choice == 4:
-                print(f"There have been {user_one_bank.total_withdraws} withdraws.")
-                if option_choice == 4 and user_two != None:
-                    print(f"There have been {user_two_bank.total_withdraws} withdraws.")
+        elif option_choice == 3:
+            print(user_one_bank.deposit())
+            if option_choice == 3 and user_two != None:
+                dep = input(f"{user_two.name} would you like to deposit? Yes or No: ")
+                if dep.lower() == 'yes':
+                    print(user_two_bank.deposit())
+        elif option_choice == 4:
+            print(f"There have been {user_one_bank.total_withdraws} withdraws.")
+            if option_choice == 4 and user_two != None:
+                print(f"There have been {user_two_bank.total_withdraws} withdraws.")
 
-            elif option_choice == 5:
-                print(f"There have been {user_one_bank.total_deposits} deposits.")
-                if option__choce == 5 and user_two != None:
-                    rprint(f"There have been {user_two_bank.total_deposits} deposits.")
-            elif option_choice == 6:
-                print("Thank you for banking with us.")
-                return False
-                break
-            else:
-                print("Please choose a number from 1-6.")
+        elif option_choice == 5:
+            print(f"There have been {user_one_bank.total_deposits} deposits.")
+            if option_choice == 5 and user_two != None:
+                print(f"There have been {user_two_bank.total_deposits} deposits.")
+        elif option_choice == 6:
+            print("Thank you for banking with us.")
+            return False
+            break
+        else:
+            print("Please choose a number from 1-6.")
+
 def bank_creation(name):
     balance = float(input(f"{name.name.title()}, how much money do you have? "))
     return balance
 
+
 while True:
     print("Welcome to ABC Bank")
     name = input("Enter your name: ")
-    age = int("Enter your age: ")
+    age = int(input("Enter your age: "))
     user_one = User(name, age)
     user_two = None
-    new_user = input("Would you like to register a new person? Type 'No' to create your bank")
-    if new.user.lower() == 'yes':
+    new_user = input("Would you like to register a new person? Type 'No' to create your bank: ")
+    if new_user.lower() == 'yes':
         name = input("Enter the second person's name: ")
-        age = int("Enter the second person's age: ") 
+        age = int(input("Enter the second person's age: "))
         user_two = User(name, age)
         print("Thank you for registering 2 people. Please create your bank accounts.")
         user_one_balance = bank_creation(user_one.name)
-        user_two_balance = bank_creation(user_tw.name)
+        user_two_balance = bank_creation(user_two.name)
         user_one_bank = Bank(user_one.name, user_one.age, user_one_balance)
+        user_two_bank = Bank(user_two.name, user_two.age, user_two_balance)
 
+        flag = options(user_two)
+        if flag == False:
+            break
 
+    else:
+        user_one_balance = bank_creation(user_one.name)
+        user_one_bank = Bank(user_one.name, user_one.age, user_one_balance)
+        flag = options(user_two)
+        if flag == False:
+            break
 
